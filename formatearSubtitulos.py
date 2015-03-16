a3b87f97afb2337adc7eb406ade1346a6166f197
John Jairo Pachon Higuera <jjairoph@gmail.com>
2015-03-16 14:41:11 -0500

    version 6

--- formatearSubtitulos.py
+++ formatearSubtitulos.py
@@ -1,6 +1,6 @@
 """============================================================================
 Programa para facilitar el formateo de los subtitulos para los videos a partir
-de el archivo transcript que da SAP. Versión 5
+de el archivo transcript que da SAP. Versión 6
 Fecha creación: 27.01.2015 John Jairo Pachon H.
 El formato del archivo srt mas sencillo es algo así:
 1
@@ -26,6 +26,7 @@ También hay otro "lenguaje" WebVTT
 
 -------------------------------------------------
 SAP me entrega el script así:
+
 00:00:10 Hi and welcome to Week 1, Learning unit 3, RDS Consumption
 and Services. In this learning unit we'd like to show you
 00:00:18 where you can find, browse, download and how to navigate
@@ -34,6 +35,18 @@ deployment solution content.
 ...
 ...
 
+O a veces así
+WEEK 2, UNIT 1
+00:00:09
+Welcome to week 2 of the SAP HANA Cloud Platform course.
+00:00:13
+In the last week, we looked into how to deploy our first application into the cloud with the HANA Cloud Platform.
+00:00:21
+In this week, we will be looking into the persistence service and how to use it.
+...
+...
+
+
 El ultimo comentario queda mal pues comienza y termina en el mismo momento
 al no haber nada despues del ultimo renglon
 En la versión 4 se puede adelantar o retrasar la sincronizacion general
@@ -41,18 +54,24 @@ para corregir problemas de tiempos mal sincronizados
 
 Esta versión debe permitir separar el archivo original en las unidades correspondientes
 El archivo original no puede ser tan grande pues esto se procesa en memoria
+
+Se crea una función (generarArchivo) para el procesamiento del archivo, completar tiempo
+
+A veces algunos caraceres especiales generan mensaje de error
+Desde el notepad++ se debe guardar el archivo como utf sin BOM
+Menu encoding - Encode in utf 8 without BOM
 ============================================================================"""
 
 import os #Para poder mostrar la ruta al archivo
 import re #Para expresiones regulares
-import mmap #Separar en archivos
 import function_utilities#Funciones creadas por mi
 from datetime import datetime, date, time, timedelta
 
 
+
 #Nombre archivo por defecto a procesar donde estan todos los subtitulos
 #de la semana
-default_file = "todo_hana_week2.txt"
+default_file = "week3hana.txt"
 
 #Sin retardo sincronizacion por defecto
 default_sync_time = 0
@@ -96,6 +115,7 @@ for match in iteratorfile:
     cadena = match.string[match.start(): match.end()]
     newdata1 = newdata1.replace(cadena, k_separador)
     cadena = cadena.replace(",", "_")
+    cadena = cadena.replace(" ", "")
     archivos.append(cadena)
 
 
@@ -115,77 +135,9 @@ for i, part in enumerate(newdata1.split(k_separador)):
 
 #Ejecutar esta parte tantas veces como archivos existan
 ##=============================================================================
-#RegEx para identificar hora con minutos y segundos Ej 03:34:10
-k_expresion = '\d\d[:]\d\d[:]\d\d'
 
 #Loop sobre los nombres de los archivos a generar
 for w in archivos:
-    filein = w + ".txt"
-    f = open(filein,'r')
-#    f = open(filein, encoding="utf8")#Toco modificar había un caracter de continuación raro
-    filedata1 = f.read()
-    f.close()
-
-    #Archivo en el que quedaran los subtitulos en formato srt
-    fileout = w + ".srt"
-    newdata = filedata1
-    p = re.compile(k_expresion)
-    iterator = p.finditer(filedata1)
-
-    #Se usa para almacenar en una lista todos los intervalos de tiempo
-    #Sirve para obtener el tiempo en el que debe terminar de verse el texto
-    #El cual corresponde a cuando comienza el siguiente.
-    p1 = re.compile(k_expresion)
-    iterator1 = p1.finditer(filedata1)
-    l = list(iterator1)
-    tamano_lista = len(l)
-
-    i = 0 #Inicializar contador
-
-    for match in iterator:
-        print(match.span())#Punto en donde se encuentra la coincidencia
-        print( match.start())#Donde comienza el string que concuerda
-        print( match.end())#Donde termina el string que concuerda
-        cadena = match.string[match.start(): match.end()]
-        partes = cadena.split(':')
-        print(partes)
-        t = datetime(100,1,1, int(partes[0]), int(partes[1]), int(partes[2]))#Inicia speak
-        siguiente = i+1
-
-        if siguiente < tamano_lista:
-            fila = l[siguiente]
-            hora2 = fila.string[fila.start(): fila.end()]
-            partes1 = hora2.split(':')
-            t1 = datetime(100,1,1, int(partes1[0]), int(partes1[1]), int(partes1[2]))#Inicia speak
-
-        t = function_utilities.sincronizar(t, sync_time)#Añade o resta sync_time segundos al tiempo inicial
-        t1 = function_utilities.sincronizar(t1, sync_time)#Añade o resta sync_time segundos al tiempo final
-
-        #Se toma la parte de hh:mm:ss y se le colocan 0 ms
-        ti = str(t)[11:19] + ",000"
-        tf = str(t1)[11:19] + ",000"
-
-        if i == 0:#Para el primer elemento
-            reemplazar = str(i)  + '\n' + ti + " --> " + tf #+ '\n' x que ahora viene en diferente renglon
-        else:
-            cadena = '\n' + cadena
-            reemplazar = '\n\n' + str(i)  + '\n' + ti + " --> " + tf #+ '\n'  x que ahora viene en diferente renglon
-
-        i = i + 1
-
-        newdata = newdata.replace(cadena, reemplazar)
-
-    mensaje =  'hay  %s  lineas de dialogo en el archivo '
-    print(mensaje %  i)
-
-    ##Abre el archivo en donde se almacenará el archivo de subtitulos
-    f2 = open(fileout,'w')
-    ##Escribe en el archivo la información sin los caracteres que se eliminaron
-    f2.write(newdata)
-    f2.close()
-    ##Mostrar en que lugar quedo el resultado
-    archivo_salida = os.getcwd() + "\\" + f2.name
-    print ("Archivo procesado:", archivo_salida)
-    #os.remove(filein)#Borrar el archivo txt una vez fue leido
+    function_utilities.generarArchivo(w, sync_time)
 
 ##=============================================================================
